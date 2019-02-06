4cb928ef35d16e450c4de07fc1e8cf45099f4dbc
Paul Newsam <paul.newsam@accenture.com>
2019-02-06 17:11:12 -0500

    Build process working for basic web component.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Import polyfills first to ensure any code dependent on them does not error.
+import "@babel/polyfill";
+import "@webcomponents/webcomponentsjs";
+
+// Import all components here.
+import "./components/x-icon";
